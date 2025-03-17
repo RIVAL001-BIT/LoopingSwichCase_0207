6a1abd2f4cac73f5f8eb83e947c28edf5a893b83
RIVAL001-BIT <adnan.r.bagu@gmail.com>
2025-03-17 20:13:22 +0700

    example fo

--- pertemuan.cpp
+++ pertemuan.cpp
@@ -8,4 +8,12 @@ int main (){
     int i;
     int perulanganwhile = 0;
     int perulanganDo = 0;
+
+    //loop with for
+    //loop fowr ascending
+    for (counter = 0; counter < 5; counter++){
+        cout << "Nilai Counter = " << counter << "TI UMY" << endl;
+    }
+    cout << "Nilai counter terakhir = " << counter << endl;
+    cout <<endl;
 }
